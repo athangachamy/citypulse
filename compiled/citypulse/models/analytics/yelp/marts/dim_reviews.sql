a7f5a29cb7dd6c4ba0e95161115c4d7b0773c755
athangachamy <athangachamy@users.noreply.github.com>
2025-04-25 16:12:02 +0000

    Deploying to gh-pages from @ athangachamy/citypulse@d75c2c835fa82538dfb36a28aea9a1abda156ec2 🚀

---
+++ compiled/citypulse/models/analytics/yelp/marts/dim_reviews.sql
@@ -0,0 +1,15 @@
+
+
+select
+    review_id,
+    user_id,
+    business_id,
+    stars,
+    review_date,
+    review,
+    useful_votes,
+    funny_votes,
+    cool_votes,
+    '2025-04-25 16:11:33.521302+00:00'::timestamp_tz as executed_at
+from raw.yelp.view_reviews
+
---
+++ compiled/citypulse/models/analytics/yelp/marts/dim_users.sql
@@ -0,0 +1,26 @@
+
+
+select
+    user_id,
+    name,
+    review_count,
+    yelping_since,
+    useful_votes,
+    funny_votes,
+    cool_votes,
+    fans,
+    average_stars,
+    compliment_hot,
+    compliment_more,
+    compliment_profile,
+    compliment_cute,
+    compliment_list,
+    compliment_note,
+    compliment_plain,
+    compliment_cool,
+    compliment_funny,
+    compliment_writer,
+    compliment_photos,
+    '2025-04-25 16:11:33.521302+00:00'::timestamp_tz as executed_at
+from raw.yelp.view_users
+
